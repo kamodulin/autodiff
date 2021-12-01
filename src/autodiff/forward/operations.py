1a3527982373664b719bf372211f69eb46aa0d92
Xuliang Guo <xuliang_guo@g.harvard.edu>
2021-12-01 16:27:22 -0500

    'added_inverse_trig'

--- src/autodiff/forward/operations.py
+++ src/autodiff/forward/operations.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from .dual import Dual
 
-__all__ = ["sin", "cos", "tan", "exp", "log", "sqrt"]
+__all__ = ["sin", "cos", "tan", "exp", "log", "sqrt", "arcsin", "arccos", "arctan", "logistic"]
 
 
 def sin(x):
@@ -33,3 +33,22 @@ def sqrt(x):
     if x.val < 0:
         raise ValueError(f"Derivative of sqrt(x) is undefined for x < 0")
     return Dual(np.sqrt(x.val), 0.5 / np.sqrt(x.val) * x.der)
+
+def arcsin(x):
+    if x.val**2 >= 1:
+        raise ValueError(f"Derivative of arcsin(x) is undefined for x = {x.val}")
+
+    return Dual(np.arcsin(x.val),x.der/(np.sqrt(1-x.val**2)))
+
+def arccos(x):
+    if x.val**2 >= 1:
+        raise ValueError(f"Derivative of arccos(x) is undefined for x = {x.val}")
+
+    return Dual(np.arccos(x.val),-x.der/(np.sqrt(1-x.val**2)))
+
+def arctan(x):
+    return Dual(np.arctan(x),x.der/(1+x.val**2))
+
+def logistic(x):
+    g = lambda z: 1/(1+np.exp(-z))
+    return Dual(g(x.val),x.der*g(x.val)*(1-g(x.val)))
