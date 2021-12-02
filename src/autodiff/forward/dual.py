850efb651b27ee71e046749a088e41b139457b65
Kamran Ahmed <k.ahmed@berkeley.edu>
2021-12-02 15:36:38 -0500

    Begin adding docstrings to Dual methods

--- src/autodiff/forward/dual.py
+++ src/autodiff/forward/dual.py
@@ -8,6 +8,25 @@ class Dual:
 
     @property
     def ndim(self):
+        """
+        Return the number of dimensions of the Dual number.
+
+        Parameters
+        ----------
+        self : Dual
+
+        Returns
+        -------
+        out : int
+            Number of dimensions.
+        
+        Examples
+        --------
+        >>> Dual(42, [1, 2]).ndim
+        2
+        >>> Dual(-5, [6.2]).ndim
+        1
+        """
         return len(self.der)
 
     @staticmethod
@@ -81,7 +100,10 @@ class Dual:
 
         Parameters
         ----------
+        self : Dual
         other : Dual
+        operand : str, optional
+
         Returns
         -------
         out : Dual, ArithmeticError, or TypeError
@@ -114,44 +136,240 @@ class Dual:
         )
 
     def __repr__(self):
+        """
+        Return a string representation of the Dual number.
+
+        Parameters
+        ----------
+        self : Dual
+
+        Returns
+        -------
+        out : str
+        """
         return f"{self.__class__.__name__}({self.val}, {np.array_repr(self.der)})"
 
     def __add__(self, other):
+        """
+        Return the sum of `self` and `other`.
+
+        Parameters
+        ----------
+        self : Dual
+        other : int, float, Dual
+        
+        Returns
+        -------
+        out : Dual
+        
+        Examples
+        --------
+        >>> Dual(42) + 5
+        Dual(47, array([1]))
+        >>> Dual(42) + Dual(1)
+        Dual(43, array([2]))
+        >>> Dual(42, [1, 2]) + Dual(1, [3, 4])
+        Dual(43, array([4, 6]))
+        """
         if other := self._compatible(other, "+"):
             return Dual(self.val + other.val, self.der + other.der)
 
     def __radd__(self, other):
+        """
+        Return the sum of two numbers, when the left operand is not a Dual
+        number.
+
+        Parameters
+        ----------
+        self : Dual
+        other : int, float
+
+        Returns
+        -------
+        out : Dual
+        
+        Examples
+        --------
+        >>> 1.2 + Dual(42)
+        Dual(43.2, array([1]))
+        >>> -3.6 + Dual(42, [1, 2])
+        Dual(38.4, array([1, 2]))
+        """
         return self + other
 
     def __sub__(self, other):
+        """
+        Returns the difference between `self` and `other`.
+
+        Parameters
+        ----------
+        self : Dual
+        other : int, float, Dual
+
+        Returns
+        -------
+        out : Dual
+
+        Examples
+        --------
+        >>> Dual(42) - 5
+        Dual(37, array([1]))
+        >>> Dual(42) - Dual(1)
+        Dual(41, array([0]))
+        >>> Dual(42, [1, 2]) - Dual(1, [3, 4])
+        Dual(41, array([-2, -2]))
+        """
         if other := self._compatible(other, "-"):
             return Dual(self.val - other.val, self.der - other.der)
 
     def __rsub__(self, other):
+        """
+        Return the difference between two numbers, when the left operand is not
+        a Dual number.
+        
+        Parameters
+        ----------
+        self : Dual
+        other : int, float
+
+        Returns
+        -------
+        out : Dual
+
+        Examples
+        --------
+        >>> 1.2 - Dual(42)
+        Dual(-40.8, array([-1.]))
+        >>> -3.6 - Dual(42, [1, 2])
+        Dual(-45.6, array([-1., -2.]))
+        """
         if other := self._compatible(other, "-"):
             return Dual(other.val - self.val, other.der - self.der)
 
     def __mul__(self, other):
+        """
+        Return the product of `self` and `other`.
+
+        Parameters
+        ----------
+        self : Dual
+        other : int, float, Dual
+
+        Returns
+        -------
+        out : Dual
+
+        Examples
+        --------
+        >>> Dual(42) * 5
+        Dual(210, array([5]))
+        >>> Dual(5.6) * Dual(1)
+        Dual(5.6, array([6.6]))
+        >>> Dual(-9, [1, 2]) * Dual(4, [2, -9])
+        Dual(-36, array([-14,  89]))
+        """
         if other := self._compatible(other, "*"):
             return Dual(self.val * other.val,
                         self.val * other.der + self.der * other.val)
 
     def __rmul__(self, other):
+        """
+        Return the product of two numbers, when the left operand is not a Dual
+        number.
+
+        Parameters
+        ----------
+        self : Dual
+        other : int, float
+
+        Returns
+        -------
+        out : Dual
+
+        Examples
+        --------
+        >>> 1.2 * Dual(42)
+        Dual(50.4, array([1.2]))
+        >>> -3 * Dual(42, [1, 2])
+        Dual(-126, array([-3., -6.]))
+        """
         return self * other
 
     def __truediv__(self, other):
+        """
+        Return the quotient of `self` and `other`.
+
+        Parameters
+        ----------
+        self : Dual
+        other : int, float, Dual
+
+        Returns
+        -------
+        out : Dual
+
+        Examples
+        --------
+        >>> Dual(42) / 5
+        Dual(8.4, array([0.2]))
+        >>> Dual(4) / Dual(5)
+        Dual(0.8, array([0.04]))
+        >>> Dual(42, [1, 2]) / Dual(1, [3, 4])
+        Dual(42.0, array([-125., -166.]))
+        """
         if other := self._compatible(other, "/"):
             return Dual(self.val / other.val,
                         (other.val * self.der - self.val * other.der) /
                         (other.val**2))
 
     def __rtruediv__(self, other):
+        """
+        Return the quotient of two numbers, when the left operand is not a Dual
+        number.
+
+        Parameters
+        ----------
+        self : Dual
+        other : int, float
+
+        Returns
+        -------
+        out : Dual
+
+        Examples
+        --------        
+        >>> 2 / Dual(4)
+        Dual(0.5, array([-0.125]))
+        >>> 2 / Dual(4, [1, 2])
+        Dual(0.5, array([-0.125, -0.25 ]))
+        """
         if other := self._compatible(other, "/"):
             return Dual(other.val / self.val,
                         (self.val * other.der - other.val * self.der) /
                         (self.val**2))
 
     def __pow__(self, other):
+        """
+        Return `self` to the power of `other`.
+
+        Parameters
+        ----------
+        self : Dual
+        other : int, float
+
+        Returns
+        -------
+        out : Dual
+
+        Examples
+        --------
+        >>> Dual(2) ** 5
+        Dual(32, array([80.]))
+        >>> Dual(2, [1]) ** Dual(3, [2])
+        Dual(8, array([23.09035489]))
+        >>> Dual(3, [1, 2]) ** Dual(2, [3, 4])
+        Dual(9, array([35.66253179, 51.55004239]))
+        """
         if other := self._compatible(other, "**"):
             der_comp_2 = other.der * np.log(
                 self.val) + other.val * (self.der / self.val)
