9a625a1dc021f462f762cb74b787df9a8e927834
Kamran Ahmed <k.ahmed@berkeley.edu>
2021-12-03 10:03:00 -0500

    Use np.isclose to test for equality in test utils

--- tests/utils.py
+++ tests/utils.py
@@ -4,16 +4,19 @@ import autodiff as ad
 
 import autodiff.reverse as adr
 
-def _equal(x, val, der, eval_der = None):
+
+def _equal(x, val, der, eval_der=None):
     if eval_der is None:
-        return x.val == val and np.all(x.der == der)
+        return np.isclose(x.val, val) and np.isclose(x.der, der).all()
     else:
-        return x.val == val and np.all(eval_der == der)
+        return np.isclose(x.val, val) and np.isclose(eval_der, der).all()
+
 
 def _compare(comparison, val, der):
     x = ad.Dual(*comparison)
     return _equal(x, val, der)
 
+
 def _compare_node(comparison, val, der, eval_der):
     x = adr.Node(*comparison)
-    return _equal(x,val,der,eval_der)
+    return _equal(x, val, der, eval_der)
