0176e941307a8b105b93ef6c60a1905379fd4c97
Kamran Ahmed <k.ahmed@berkeley.edu>
2021-12-03 10:00:51 -0500

    Include more tests for add and add pow and rpow tests

--- tests/test_node.py
+++ tests/test_node.py
@@ -9,13 +9,122 @@ from utils import _equal, _compare_node
 @pytest.mark.parametrize("val1", [0.7, 64])
 @pytest.mark.parametrize("val2", [-2, 4.2])
 def test_add_constant(val1, val2):
+    x = val1
+    y = ad.Node.constant(val2)
+    f = x + y
+    der = 0
+    eval_der = np.array([y.grad()])
+    assert _equal(f, val1 + val2, der, eval_der)
+
+    x = ad.Node.constant(val1)
+    y = val2
+    f = x + y
+    der = 0
+    eval_der = np.array([x.grad()])
+    assert _equal(f, val1 + val2, der, eval_der)
+
     x = ad.Node.constant(val1)
     y = ad.Node.constant(val2)
-    assert _equal(x + y, val1 + val2, 0, np.array([x.grad(),y.grad()]))
+    f = x + y
+    der = 0 + 0
+    eval_der = np.array([x.grad(), y.grad()])
+    assert _equal(f, val1 + val2, der, eval_der)
+
 
 @pytest.mark.parametrize("val1", [0.7, 64])
 @pytest.mark.parametrize("val2", [-2, 4.2])
 def test_add_variable(val1, val2):
+    x = val1
+    y = ad.Node(val2)
+    f = x + y
+    der = 1
+    eval_der = np.array([y.grad()])
+    assert _equal(f, val1 + val2, der, eval_der)
+
+    x = ad.Node(val1)
+    y = val2
+    f = x + y
+    der = 1
+    eval_der = np.array([x.grad()])
+    assert _equal(f, val1 + val2, der, eval_der)
+
     x = ad.Node(val1)
     y = ad.Node(val2)
-    assert _equal(x + y, val1 + val2, np.array([1.0, 1.0]), np.array([x.grad(),y.grad()]))
+    f = x + y
+    der = np.array([1, 1])
+    eval_der = np.array([x.grad(), y.grad()])
+    assert _equal(f, val1 + val2, der, eval_der)
+
+
+@pytest.mark.parametrize("val1", [0.7, 64])
+@pytest.mark.parametrize("val2", [-2, 4.2])
+def test_pow_constant(val1, val2):
+    x = val1
+    y = ad.Node.constant(val2)
+    f = x**y
+    der = 0
+    eval_der = np.array([y.grad()])
+    assert _equal(f, val1**val2, der, eval_der)
+
+    x = ad.Node.constant(val1)
+    y = val2
+    f = x**y
+    der = 0
+    eval_der = np.array([x.grad()])
+    assert _equal(f, val1**val2, der, eval_der)
+
+    x = ad.Node.constant(val1)
+    y = ad.Node.constant(val2)
+    f = x**y
+    der = np.array([0, 0])
+    eval_der = np.array([x.grad(), y.grad()])
+    assert _equal(f, val1**val2, der, eval_der)
+
+
+@pytest.mark.parametrize("val1", [-0.7, -64])
+@pytest.mark.parametrize("val2", [-2.1, 4.2])
+def test_pow_invalid(val1, val2):
+    with pytest.raises(ValueError):
+        x = val1
+        y = ad.Node.constant(val2)
+        _ = x**y
+
+        x = ad.Node.constant(val1)
+        y = val2
+        _ = x**y
+
+        x = ad.Node.constant(val1)
+        y = ad.Node.constant(val2)
+        _ = x**y
+
+    with pytest.raises(ZeroDivisionError):
+        x = ad.Node.constant(0)
+        y = -2.1
+        _ = x**y
+
+
+@pytest.mark.parametrize("val1", [0.7, 64])
+@pytest.mark.parametrize("val2", [-2, 4.2])
+def test_pow_variable(val1, val2):
+    x = val1
+    y = ad.Node(val2)
+    f = x**y
+    der = val1**(val2) * np.log(val1)
+    eval_der = np.array([y.grad()])
+    assert _equal(f, val1**val2, der, eval_der)
+
+    x = ad.Node(val1)
+    y = val2
+    f = x**y
+    der = val1**(val2) * val2 / val1
+    eval_der = np.array([x.grad()])
+    assert _equal(f, val1**val2, der, eval_der)
+
+    x = ad.Node(val1)
+    y = ad.Node(val2)
+    f = x**y
+    x_grad = val1**(val2) * val2 / val1
+    y_grad = val1**(val2) * np.log(val1)
+    der = np.array([x_grad, y_grad])
+    eval_der = np.array([x.grad(), y.grad()])
+    assert _equal(f, val1**val2, der, eval_der)
--- tests/test_reverse_operations.py
+++ tests/test_reverse_operations.py
@@ -5,18 +5,23 @@ import autodiff.reverse as ad
 
 from utils import _equal
 
+
 @pytest.mark.parametrize("val", [1, -6.2])
 def test_sin_constant(val):
     x = ad.Node.constant(val)
     out = ad.sin(x)
-    assert _equal(out, np.sin(val), np.cos(val) * 0, np.array([x.grad()]))
+    der = np.cos(val) * 0
+    eval_der = np.array([x.grad()])
+    assert _equal(out, np.sin(val), der, eval_der)
 
 
-@pytest.mark.parametrize("val",[0.7,-64])
+@pytest.mark.parametrize("val", [0.7, -64])
 def test_sin_variable(val):
-	x = ad.Node(val)
-	out = ad.sin(x)
-	assert _equal(out, np.sin(val), np.cos(val), np.array([x.grad()]))
+    x = ad.Node(val)
+    out = ad.sin(x)
+    der = np.cos(val)
+    eval_der = np.array([x.grad()])
+    assert _equal(out, np.sin(val), der, eval_der)
 
 
 @pytest.mark.parametrize("val", [0.7, -64])
@@ -25,7 +30,8 @@ def test_sin_variable(val):
 def test_sin_multichildren(val, der, child_val):
     x = ad.Node(val)
     child = ad.Node(child_val)
-    x._addChildren(der,child)
+    x._addChildren(der, child)
     out = ad.sin(x)
-    assert _equal(out, np.sin(val), np.cos(val) + der, np.array([x.grad()]))
-
+    der = np.cos(val) + der
+    eval_der = np.array([x.grad()])
+    assert _equal(out, np.sin(val), der, eval_der)
