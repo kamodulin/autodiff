1d08528cd65ec62a1b78e880d84d07b8d9a43c85
Xuliang Guo <xuliang_guo@g.harvard.edu>
2021-12-06 16:00:48 -0500

    'implemented_finite_difference_method_for_integreation_testing'

--- tests/utils.py
+++ tests/utils.py
@@ -21,3 +21,11 @@ def _compare_node(comparison, val, der, eval_der):
     x = adr.Node(comparison)
 
     return (np.isclose(x.val, val) and np.isclose(eval_der, der).all()) if eval_der is not None else (np.isclose(x.val, val) and eval_der == None)
+
+def fdn(g, x, epi = 1e-4):
+    f = lambda y: g(*y)
+    d = len(x)
+    mat = np.eye(d) * epi
+    xplus =  x + mat
+    xminus = x - mat
+    return (np.apply_along_axis(f,1,xplus) - np.apply_along_axis(f,1,xminus))/(2*epi)
